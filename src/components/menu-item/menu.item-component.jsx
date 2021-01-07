ebd0632ba0af394c41487c99d4e378839abfff1f
RohanRony <rohanrony@hotmail.com>
2021-01-07 21:51:44 +0300

    withRouter()

--- src/components/menu-item/menu.item-component.jsx
+++ src/components/menu-item/menu.item-component.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 import './menu.item-styles.scss'
 
-const MenuItem = ({title,imageUrl,size}) =>
+const MenuItem = ({title,imageUrl,size,history}) =>
 (
     <div className={`${size} menu-item`}>
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
@@ -16,4 +17,4 @@ const MenuItem = ({title,imageUrl,size}) =>
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
