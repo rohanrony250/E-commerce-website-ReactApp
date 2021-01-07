4fabd689b7adbaeb82cae602fe2948badf6cddfb
RohanRony <rohanrony@hotmail.com>
2021-01-07 22:26:00 +0300

    init

--- src/components/menu-item/menu.item-component.jsx
+++ src/components/menu-item/menu.item-component.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import {withRouter} from 'react-router-dom'
 import './menu.item-styles.scss'
 
-const MenuItem = ({title,imageUrl,size,history}) =>
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) =>
 (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}> 
+        {/* history method finds the url to goto by using the match method within so that react knows where to go for each item in the menu, eg: hats will lead to hats page */}
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className='item-content'>
             <h1 className='title'>
