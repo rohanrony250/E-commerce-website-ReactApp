3841341c3c12fc7fe09a9e8ee32fba90c429f2ff
RohanRony <rohanrony@hotmail.com>
2021-01-08 23:58:45 +0300

    state

--- src/pages/shop/shop-component.jsx
+++ src/pages/shop/shop-component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import SHOP_ITEMS from './shop-items'
 class shop extends React.Component
 {
     constructor(props)
     {
-        super()
-        this.state =
+        super(props)
+        this.state = {
+            collections: SHOP_ITEMS
+        }
         
     }
 }
