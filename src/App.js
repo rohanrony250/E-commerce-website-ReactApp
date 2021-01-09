d3291683be073c9b5dd4514a471f63b1cf072f36
RohanRony <rohanrony@hotmail.com>
2021-01-10 01:20:45 +0300

    created header component and applied basic styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HomePage from './pages/homepage/homepage-component';
 import {Switch, Route} from 'react-router-dom';
 import Shop_Component from './pages/shop/shop-component'
+import HeaderComponent from './components/header/header-component'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <Switch>
         <Route exact path='/' component={HomePage}/> 
         {/*here only {HomePage} gets access to props of route and not its children, to solve issue we use withRouter() */}
---
+++ src/components/header/header-component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './header-styles.scss'
+import {Link} from 'react-router-dom'
+import { ReactComponent as Logo } from '../../resources/crown.svg'
+const HeaderComponent = () =>
+(
+    <div className='header'>
+        <Link to='/' className='logo-container'>
+            <Logo className='logo'></Logo>
+        </Link>
+        <div className='options'>
+            <Link to='/shop' className='option'>
+                SHOP
+            </Link>
+            <Link to='/contact' className='option'>
+                CONTACT
+            </Link>
+        </div>
+    </div>
+)
+
+export default HeaderComponent
