030ee6362b81f31fdc3ac8202d97b901635d6ebe
RohanRony <rohanrony@hotmail.com>
2021-01-11 01:20:04 +0300

    basic functionality of sign-in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -13,18 +13,51 @@ class SignIn extends React.Component
         }
     }
 
+    handleSubmit = e =>
+    {
+        e.preventDefault();
+        this.setState({email:'',password:''})
+    }
+
+    handleChange = e =>
+    {
+        const{name,value} = e.target
+        this.setState({[name]: value});
+    }
+
     render()
     {
         return(
             <div className='sign-in'>
-                <h2>I already have an account</h2>
+                <h1>LOGIN</h1>
+                <h3>I already have an account</h3>
                 <span>Sign-In with existing account</span>
             
-            <form>
-                <input name='email' value={this.state.email} required />
-                <label>Email</label>
-                <input name='password' value={this.state.password} required />
-                <label>Password</label>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    name='email'
+                    placeholder='EMAIL' 
+                    type='email' 
+                    value={this.state.email} 
+                    onChange={this.handleChange}
+                    required 
+                    
+                />
+                
+                <input 
+                    name='password'
+                    placeholder='PASSWORD' 
+                    type='password' 
+                    value={this.state.password} 
+                    onChange={this.handleChange}
+                    required 
+                    
+                />
+                
+                <input 
+                    type="submit" 
+                    value='SUBMIT FORM'/>
+            
             </form>
             
             </div>
--- src/pages/login/login-component.jsx
+++ src/pages/login/login-component.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import './login-styles.scss'
-
+import React from 'react';
+import './login-styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
 const LoginPage = () =>
 (
-    <div className='login'>SIGN IN</div>
+    <div className='login'> 
+        <SignIn />
+    </div>
 )
 
 export default LoginPage
