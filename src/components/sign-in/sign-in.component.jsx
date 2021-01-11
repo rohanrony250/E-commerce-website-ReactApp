8fb835574a083665b200ac11d8411e7efb347fb8
RohanRony <rohanrony@hotmail.com>
2021-01-11 17:36:06 +0300

    form-input

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -32,34 +32,30 @@ class SignIn extends React.Component
                 <h1>LOGIN</h1>
                 <h3>I already have an account</h3>
                 <span>Sign-In with existing account</span>
+
             
-            <form onSubmit={this.handleSubmit}>
-                <input 
-                    name='email'
-                    placeholder='EMAIL' 
-                    type='email' 
-                    value={this.state.email} 
-                    onChange={this.handleChange}
-                    required 
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        name='email'
+                        placeholder='EMAIL' 
+                        type='email' 
+                        value={this.state.email} 
+                        onChange={this.handleChange}
+                        required
+                    />
                     
-                />
-                
-                <input 
-                    name='password'
-                    placeholder='PASSWORD' 
-                    type='password' 
-                    value={this.state.password} 
-                    onChange={this.handleChange}
-                    required 
+                    <input 
+                        name='password'
+                        placeholder='PASSWORD' 
+                        type='password' 
+                        value={this.state.password} 
+                        onChange={this.handleChange}
+                        required 
+                    />
                     
-                />
-                
-                <input 
-                    type="submit" 
-                    value='SUBMIT FORM'/>
-            
-            </form>
-            
+                    <button className='btn btn-primary' type='submit'>LOGIN</button>
+                    
+                </form>
             </div>
         )
     }
