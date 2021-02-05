4a7546fc090b1ebb2ebd5bcb589d2ed494d7cdc1
RohanRony <rohanrony@hotmail.com>
2021-02-05 22:26:22 +0300

    connected cart reducer and completed adding cart items

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown-styles.scss'
 import { connect } from 'react-redux'
+import CartItem from '../cart-item/cart-item.component'
 
-
-const CartDropdown = () =>
+const CartDropdown = ({ cartItems }) =>
 (
     <div className='cart-dropdown text-white'>
-        <div className='cart-items'></div>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+            }
+        </div>
         <CustomButton type='button' styles='btn btn-dark button'>GO TO CHECKOUT</CustomButton>
     </div>
 )
