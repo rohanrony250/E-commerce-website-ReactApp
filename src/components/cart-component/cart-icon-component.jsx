8a7a59644e2de7d74b2c806847f76d2c7bfe0b19
RohanRony <rohanrony@hotmail.com>
2021-02-07 21:04:53 +0300

    cart count selector implemented

--- src/components/cart-component/cart-icon-component.jsx
+++ src/components/cart-component/cart-icon-component.jsx
@@ -5,11 +5,11 @@ import { ReactComponent as ShoppingIcon } from '../../resources/cart-icon.svg';
 import './cart-icon-styles.scss';
 
 
-const CartComponent = ({ToggleCartHidden}) => (
+const CartComponent = ({ToggleCartHidden, itemCount}) => (
 
     <div className='cart-icon' onClick = {ToggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'> 0 </span>
+        <span className='item-count'> {itemCount} </span>
     </div>
 )
 
@@ -19,4 +19,11 @@ const mapDispatchToProps = dispatch => (
     }
 )
 
-export default connect(null, mapDispatchToProps)(CartComponent)
+const mapStateToProps = ({cart: {cartItems}}) => (
+    {
+        itemCount : cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,0)
+        // here everything before '0' is the total value that we need , and '0' is the initial value/currentvalue if an existing value is not passed in.
+    }
+) 
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartComponent)
