eca2c37b442fb090b2516d73706a7a30e8c7d378
RohanRony <rohanrony@hotmail.com>
2021-02-08 21:58:27 +0300

    updated cart-dropdown with memoized selectors to reduce app render timing

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -3,6 +3,7 @@ import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown-styles.scss'
 import { connect } from 'react-redux'
 import CartItem from '../cart-item/cart-item.component'
+import { selectCartItems } from '../../redux/cart/cart-selector'
 
 const CartDropdown = ({ cartItems }) =>
 (
@@ -20,11 +21,14 @@ const CartDropdown = ({ cartItems }) =>
     </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) =>
+const mapStateToProps = state =>
 (
+    
     {
-        cartItems
+        cartItems: selectCartItems(state)
     }
+    
 )
-
+// ({ cart: { cartItems } }) , without selector
 export default connect(mapStateToProps)(CartDropdown)
+
