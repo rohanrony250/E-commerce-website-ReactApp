5a306365e960f886c184d9ba66043be98768a86b
RohanRony <rohanrony@hotmail.com>
2021-02-09 23:57:01 +0300

    initiated checkout-component

---
+++ src/pages/checkout/checkout-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './checkout-styles.scss'
+
+const CheckoutComponent = () =>
+(
+    <div>
+        checkout page.
+    </div>
+)
+
+
+export default CheckoutComponent
