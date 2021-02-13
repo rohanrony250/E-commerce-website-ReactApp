1d5b928a6e7d13974b5dddeac16c26100f1523cb
RohanRony <rohanrony@hotmail.com>
2021-02-13 22:13:48 +0300

    checkoutpage

--- src/pages/checkout/checkout-component.jsx
+++ src/pages/checkout/checkout-component.jsx
@@ -3,8 +3,34 @@ import './checkout-styles.scss'
 
 const CheckoutComponent = () =>
 (
-    <div>
-        checkout page.
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>
+                    Product
+                </span>
+            </div>
+            <div className='header-block'>
+                <span>
+                    Description
+                </span>
+            </div>
+            <div className='header-block'>
+                <span>
+                    Quantity
+                </span>
+            </div>
+            <div className='header-block'>
+                <span>
+                    Price
+                </span>
+            </div>
+            <div className='header-block'>
+                <span>
+                    Remove
+                </span>
+            </div>
+        </div>
     </div>
 )
 
